7e8cac51c5fd32312bcd778ecee26dccecb8fc9c
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-03-02 13:28:21 -0300

    Stage 3

--- Words Virtuoso/task/src/wordsvirtuoso/Main.kt
+++ Words Virtuoso/task/src/wordsvirtuoso/Main.kt
@@ -1,6 +1,7 @@
 package wordsvirtuoso
 
 import java.io.File
+import java.util.*
 import kotlin.system.exitProcess
 
 fun main(vararg args: String) {
@@ -28,23 +29,24 @@ The second argument is the filename of the text file containing the candidate wo
     }
 
     //Check the file containing all words for invalid words. If present, print Error: <Number of invalid words> invalid words were found in the <All words filename> file. and exit;
-    val lines = file.readLines()
-    val invalidWords = lines.count { !checkString(it) }
+    val allWords = file.readLines().map { it.trim().lowercase(Locale.getDefault()) }
+
+    val invalidWords = allWords.count { !checkString(it) }
     if (invalidWords > 0) {
         println("Error: $invalidWords invalid words were found in the ${args[0]} file.")
         exitProcess(1)
     }
 
     //Check the file containing the candidate words for invalid words. If present, print Error: <Number of invalid words> invalid words were found in the <Candidates filename> file. and exit;
-    val lines2 = file2.readLines()
-    val invalidWords2 = lines2.count { !checkString(it) }
+    val candidateWords = file2.readLines().map { it.trim().lowercase(Locale.getDefault()) }
+    val invalidWords2 = candidateWords.count { !checkString(it) }
     if (invalidWords2 > 0) {
         println("Error: $invalidWords2 invalid words were found in the ${args[1]} file.")
         exitProcess(1)
     }
 
     //Check whether each candidate word is included in the file with all words. If any candidate words don't comply with that, print Error: <Number of candidates not included> candidate words are not included in the <All words filename> file. and exit;
-    val notIncluded = lines2.count { !lines.contains(it.trim()) }
+    val notIncluded = candidateWords.count { !allWords.contains(it) }
     if (notIncluded > 0) {
         println("Error: $notIncluded candidate words are not included in the ${args[0]} file.")
         exitProcess(1)
